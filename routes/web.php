77f770f4d9a1de5adfcc61426e59ec48d029a0bf
kevinchristianto <christiantok12@gmail.com>
2025-07-12 20:38:43 +0700

    hehe

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WaliMuridController;
-use Illuminate\Support\Facades\Route;s
+use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function() {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
